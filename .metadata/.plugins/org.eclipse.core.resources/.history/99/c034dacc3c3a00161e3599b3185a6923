19aab79f028292be2a70ab895d9ea2bf2f5d7bfc
Liang Li <leeo1116@gmail.com>
2016-06-25 17:23:37 -0500

    06/25

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/99/c034dacc3c3a00161e3599b3185a6923
@@ -0,0 +1,5 @@
+package basics.tutorial;
+
+public class ScannerTest {
+
+}
--- Java Coursera PKU/src/basics/tutorial/ScannerTest.java
+++ Java Coursera PKU/src/basics/tutorial/ScannerTest.java
@@ -1,12 +1,12 @@
 package basics.tutorial;
 import java.util.Scanner;
+
 public class ScannerTest {
 	public static void main( String[] args ){
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Please input a number ");
 		int a = scanner.nextInt();
 		System.out.printf("The square of %d is %d", a, a*a);
-		
 	}
 
 }
